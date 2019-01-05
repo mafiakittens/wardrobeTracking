e17e80da685da786d42fe05de63c480744a22aa9
Celia Ludwinski <celia.ludwinski@gmail.com>
2019-01-05 17:55:58 +1000

    Initialize structure

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='A web interface tool to track daily outfits, analyze cost per wear, and visualize results. ',
+    author='Celia Ludwinski',
+    license='',
+)
